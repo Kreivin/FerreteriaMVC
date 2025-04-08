15c19c8def593be9c352115577cd0a286f0f4200
Kreivin <estudiantekreivin@gmail.com>
2025-04-08 10:42:46 -0600

    Add files via upload

--- frecuenciaAVC/src/DAO/CategoriaDAO.java
+++ frecuenciaAVC/src/DAO/CategoriaDAO.java
@@ -8,6 +8,9 @@ import Util.ConexionDB;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -29,17 +32,40 @@ public void crearCategoria(Categoria categoria) throws SQLException {
         stmt.executeUpdate();
     }
 }
+// Método para leer todas las categorías
+public List<Categoria> leerTodasCategorias() throws SQLException {
+String sql = "SELECT * FROM Categorias";
+List<Categoria> categorias = new ArrayList<>();
+try (Connection c = ConexionDB.getConnection();
+PreparedStatement stmt = c.prepareStatement(sql);
+ResultSet rs = stmt.executeQuery()) {
+while (rs.next()) {
+Categoria categoria = new Categoria();
+categoria.setIdCategoria(rs.getInt("id_categoria"));
+categoria.setNombreCategoria(rs.getString("nombre_categoria"));
+categoria.setDescripcionCategoria(rs.getString("descripcion_categoria"));
+categorias.add(categoria);
+}
+}
+return categorias;
+}
 
 public static void main(String[] args) {
     try {
         CategoriaDAO dao = new CategoriaDAO();
-        Categoria c1 = new Categoria();
-        c1.setNombreCategoria("Construcción");
-        c1.setDescripcionCategoria("Herramientas de cosntrucción");
-        dao.crearCategoria(c1);
-        System.out.println("Categoría creada con éxito!");
+      // Leer y mostrar todas las categorías
+List<Categoria> categorias = dao.leerTodasCategorias();
+System.out.println("\nLista de categorías:");
+for (Categoria cat : categorias) {
+System.out.println("ID: " + cat.getIdCategoria() +
+
+", Nombre: " + cat.getNombreCategoria() +
+", Descripción: " + cat.getDescripcionCategoria());
+
+}
     } catch (SQLException e) {
         System.err.println("Error: " + e.getMessage());
     }
 }
+
 }
