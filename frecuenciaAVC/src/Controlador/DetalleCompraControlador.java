e95429d7aa7a6d5eb3bf00427d617ed142219994
Creivin <estudiantekreivin@gmail.com>
2025-04-24 09:53:49 -0600

    cambios del controlador

---
+++ frecuenciaAVC/src/Controlador/DetalleCompraControlador.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controlador;
+import DAO.CompraDAO;
+import DAO.DetalleCompraDAO;
+import Modelo.Compra;
+import Modelo.DetalleCompra;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author kreiv
+ */
+public class DetalleCompraControlador {
+    
+    
+}
--- frecuenciaAVC/src/DAO/CompraDAO.java
+++ frecuenciaAVC/src/DAO/CompraDAO.java
@@ -12,26 +12,35 @@ import java.util.Date;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
+import java.sql.Statement;
 /**
  *
  * @author kreiv
  */
 public class CompraDAO {
-    public void crearCompra(Compra compra) throws SQLException {
+    public int crearCompra(Compra compra) throws SQLException {
     String sql = """
         INSERT INTO Compras (
             id_empleado, 
             fecha_compra, 
             total_compra
         ) VALUES (?, ?, ?)""";
+        int generatedId = -1;
     
     try (Connection c = ConexionDB.getConnection();
-         PreparedStatement stmt = c.prepareStatement(sql)) {
+         PreparedStatement stmt = c.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS)) {
         stmt.setInt(1, compra.getIdEmpleado());
         stmt.setDate(2, new java.sql.Date(compra.getFechaCompra().getTime()));
         stmt.setFloat(3, compra.getTotalCompra());
         stmt.executeUpdate();
+        try (ResultSet rs = stmt.getGeneratedKeys()){
+        if (rs.next()){
+            generatedId = rs.getInt(1);
+        }
     }
+    }
+    
+    return generatedId;
 }
     public List<Compra> leerTodasCompras() throws SQLException {
         String sql = "SELECT * FROM Compras";
@@ -83,7 +92,7 @@ public class CompraDAO {
         compra.setIdEmpleado(2);
         compra.setFechaCompra(new java.util.Date());
         compra.setTotalCompra(1500.50f);
-        dao.actualizarCompra(compra);
+        dao.crearCompra(compra);
         System.out.println("Compra actualizada.");
         
         // Eliminar una compra
